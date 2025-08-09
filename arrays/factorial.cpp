c29f217c5409d3e089c43175c9b2e903bcce326e
Arjun c <arjunhemanth6@gmail.com>
2025-08-10 01:02:28 +0530

    Update factorial.cpp
    Added factorial program in math_problems

--- arrays/factorial.cpp
+++ arrays/factorial.cpp
@@ -1,1 +1,16 @@
-// My first array problem 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    long long fact = 1;
+
+    for(int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+
+    cout << "Factorial of " << n << " = " << fact << endl;
+    return 0;
+}
